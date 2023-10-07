f9da73eb824ad71ea79535724842b3dea1f39f43
faturoti <abfat049@gmail.com>
2023-10-07 11:12:40 -0700

    Question 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main: This dispalys the string
+ *
+ * Return: returns 0 to end main
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retrun(0);
+}
