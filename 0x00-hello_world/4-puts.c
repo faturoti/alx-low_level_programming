341a5466e974d43770d55fd8fcc2c0624a610bac
faturoti <abfat049@gmail.com>
2023-10-07 10:39:18 -0700

    Question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
- *puts prints out the value inputed
- */
-int main (void)
+/**
+ * main - prints out the value inputed 
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
-
