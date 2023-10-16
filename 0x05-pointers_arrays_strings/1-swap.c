f284f2ee8642374e903519368132bde75532bcb5
faturoti <abfat049@gmail.com>
2023-10-16 14:37:53 -0700

    Question 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - This function swaps parameters
+ * @a: First parameter
+ * @b: Second parameter
+ *
+ * Return o
+ */
+void swap_int(int *a, int *b)
+{
+	int tem = *a;
+	*a = *b;
+	*b = tem;
+}
