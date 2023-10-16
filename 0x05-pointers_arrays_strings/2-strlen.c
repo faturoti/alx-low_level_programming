43d8ca46820d3ce3f951a5c59f0fa2075c4b0a0a
faturoti <abfat049@gmail.com>
2023-10-16 15:04:45 -0700

    Question 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _strlen - Prints out length of string
- * @s: String to check
+ * @str: String to check
  *
- * Return length of string
+ * Return: length of string
  */
 size_t _strlen(const char *str)
 {
