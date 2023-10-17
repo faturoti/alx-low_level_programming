316055c0cbdf124407aa86421b89038d603d34fa
faturoti <abfat049@gmail.com>
2023-10-17 14:43:53 -0700

    Question 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,14 +6,14 @@
  *
  * Return A pointer to the destination string @dest
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy (char *dest, const char *src)
 {
 	int index = 0;
 
 	while (src[index])
 	{
 		dest[index] = src[index];
-		index++
+		index++;
 	}
 	
 	return (dest);
