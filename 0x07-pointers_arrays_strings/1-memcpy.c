fe738c64027a38049e23980c311395e68c53100f
faturoti <abfat049@gmail.com>
2023-10-23 14:57:11 -0700

    Question 3&4

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memcpy - copies memory area
  * @dest: where to copy the memory to
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: string to be searched
+ * @accept: string to be used
  *
+ * Return: a pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+	for (; s[i] != '\0' && _strchr(accept, s[i]) == NULL; i++)
+		;
+
+	if (s[i] == '\0')
+		return (NULL);
+	else
+		return (s + i);
+}
