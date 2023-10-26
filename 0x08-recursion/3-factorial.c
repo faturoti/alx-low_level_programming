ddcef6ead14c184f2769488e9bc912cebc3c2758
faturoti <abfat049@gmail.com>
2023-10-26 14:12:10 -0700

    Question 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,14 @@
 /**
  * factorial - factorial of a given number
  * @n: pointer block of memory to fill
- 
+ *
  * Return: factorial
  */
 
-int factorial (int n)
+int factorial(int n)
 {
 	if (n == 0)
-		return(1);
+		return (1);
 	else if (n < 0)
 	{
 		return (-1);
