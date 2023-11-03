aa444478ce7e077a4c32b00de82e10947e934819
faturoti <abfat049@gmail.com>
2023-11-03 16:42:51 -0700

    Question 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,21 +10,23 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	 char *block;
-	 unsigned int i;
+	char *block;
+	unsigned int i;
 
-	 if (nmemb == 0 || size == 0)
-		 return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-	 block = malloc(nmemb * size);
+	block = malloc(nmemb * size);
 
-	 if (block != NULL)
-	 {
-		 for (i = 0; i < (nmemb * size); i++)
-			 block[i] = 0;
-		 
-		 return (block);
-	 }
-	 else
-		 return (NULL);
+	if (block != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+		{
+			block[i] = 0;
+		}
+
+		return (block);
+	}
+	else
+		return (NULL);
 }
