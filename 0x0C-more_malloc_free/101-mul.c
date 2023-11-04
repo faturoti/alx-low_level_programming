306f420a6947aec74c29387ea3f5d27d3bc0ec32
faturoti <abfat049@gmail.com>
2023-11-03 17:51:35 -0700

    Question 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strlen - Finds the length of a string
+ * check_string - Finds the length of a string
  * @str: The string to be measured
  *
  * Return: The length of the string
  */
 
-bool check_string(char *str)
+int check_string(char *str)
 {
 	int i;
 
-	for(i = 0; str[i]; i++)
+	for (i = 0; str[i]; i++)
 	{
 		if (str[i] < '0' || str[i] > '9')
-			return (false);
+			return (1);
 	}
 
-	return (true);
+	return (2);
 }
 
 /**
@@ -42,15 +42,19 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!(check_string(argv[i]))
-				printf("Error\n");
-				return (98);
+		if (check_string(argv[i]) == 1)
+		{
+			printf("Error\n");
+			return (98);
+		}
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 
-	res = mul(a, b);
-	printf("%d", res);
+	res = a * b;
+	printf("%ld", res);
+	printf("\n");
 
+	return (0);
 }
