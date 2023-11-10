136c0adb0ac5211ebff7d34d154fa5da1a418a26
faturoti <abfat049@gmail.com>
2023-11-10 15:27:19 -0800

    Question4

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,10 +2,6 @@
 #define CALC_H
 
 /**
- * Desc: Header file containing all structures and
- */
-
-/**
  * struct op - A struct op.
  * @op: The operator.
  * @f: The associated function.
@@ -15,7 +11,7 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -18,7 +18,7 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - Returns the difference of two numbers.
+ * op_mul - Returns the difference of two numbers.
  * @a: The first number.
  * @b: The second number.
  *
