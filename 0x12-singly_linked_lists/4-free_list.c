9d05353969027d1142d31af60988f86eeafeb530
faturoti <abfat049@gmail.com>
2023-11-16 14:44:02 -0800

    Good

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: a pointer to the head no
+ *
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(head->str);
+	}
+
+	head = head->next;
+	free(tmp);
+}
